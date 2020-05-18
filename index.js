ad5c010b63fc54c5f7b809a30725bae2e5b3b6ba
Cranekick50 <michael.srithapin@gmail.com>
2020-05-17 20:23:40 -0500

    adjusted event title to not have gray background and centered text

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 var containerEl = document.querySelector(".container")
-var currentDayEl = document.getElementById("#currentDay")
+var currentDayEl = document.getElementById('currentDay')
+var myText = document.querySelectorAll("eventCol")
+
+var moment = require('moment-timezone');
+moment().tz("America/Los_Angeles").format();
+
+var myText = document.createElement("INPUT");
+myText.setAttribute("type", "text");
+
 
 
